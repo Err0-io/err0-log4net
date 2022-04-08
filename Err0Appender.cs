b21196e8345359f559b06f369ba2dd586efd7389
Ben Gould <ben+dev@amb1ent.org>
2022-04-08 12:36:48 +0200

    log4net implementation of log4j2 adapter

--- Err0Appender.cs
+++ Err0Appender.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Text.RegularExpressions;
+using System.Threading;
 using log4net.Appender;
 using log4net.Core;
 using Newtonsoft.Json.Linq;
@@ -10,36 +13,125 @@ namespace err0.log4net
 {
     public class Err0Appender : AppenderSkeleton
     {
-        public Err0Appender() : base()
+        public Err0Appender()
         {
             token = ConfigurationManager.AppSettings["err0.token"];
             url = ConfigurationManager.AppSettings["err0.url"];
+            bulkLogApi = new Uri(url + "~/api/bulk-log");
             realm_uuid = ConfigurationManager.AppSettings["err0.realm_uuid"];
             prj_uuid = ConfigurationManager.AppSettings["err0.prj_uuid"];
             pattern = new Regex(ConfigurationManager.AppSettings["err0.pattern"],
                 RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            thread = new Thread(new Err0Thread(this).Loop);
+            thread.IsBackground = true;
+            thread.Start();
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(CurrentDomain_ProcessExit);
+        }
+
+        void CurrentDomain_ProcessExit(object sender, EventArgs e)
+        {
+            stopped = true;
+            while (pollQueue())
+            {
+            }
         }
 
         private readonly String token;
         private readonly String url;
+        private readonly Uri bulkLogApi;
         private readonly String realm_uuid;
         private readonly String prj_uuid;
         private readonly Regex pattern;
+
+        private bool stopped = false;
+        private readonly Thread thread;
+
+        public sealed class Err0Thread
+        {
+            public Err0Thread(Err0Appender appender)
+            {
+                this.appender = appender;
+            }
+
+            private readonly Err0Appender appender;
+            
+            public void Loop()
+            {
+                for (; !appender.stopped;)
+                {
+                    if (!Err0Http.canCall())
+                    {
+                        Thread.Yield();
+                    }
+                    else
+                    {
+                        bool wasEmpty = appender.pollQueue();
+                        if (wasEmpty)
+                        {
+                            Thread.Yield();
+                        }
+                    }
+                }
+            }
+        }
         
         public sealed class Err0Log {
-            public Err0Log(string error_code, long ts, string message, JObject metadata) {
+            internal Err0Log(string error_code, long ts, string message, JObject metadata) {
                 this.error_code = error_code;
                 this.ts = ts;
                 this.message = message;
                 this.metadata = metadata;
             }
-            public string error_code;
-            public long ts;
-            public string message;
-            public JObject metadata;
+            public readonly string error_code;
+            public readonly long ts;
+            public readonly string message;
+            public readonly JObject metadata;
         }
 
         private readonly ConcurrentQueue<Err0Log> queue = new ConcurrentQueue<Err0Log>();
+
+        private bool pollQueue()
+        {
+            List<Err0Log> list = new List<Err0Log>();
+            Err0Log logEvent = null;
+            for(;;)
+            {
+                if (queue.TryDequeue(out logEvent))
+                {
+                    list.Add(logEvent);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (list.Count > 0)
+            {
+                JObject bulkLog = new JObject();
+                bulkLog.Add("realm_uuid", realm_uuid);
+                bulkLog.Add("prj_uuid", prj_uuid);
+                JArray logs = new JArray();
+                foreach (Err0Log log in list)
+                {
+                    JObject o = new JObject();
+                    o.Add("error_code", log.error_code);
+                    o.Add("ts", "" + log.ts);
+                    o.Add("msg", log.message);
+                    o.Add("metadata", log.metadata);
+                    logs.Add(o);
+                }
+                bulkLog.Add("logs", logs);
+                Err0Http.call(bulkLogApi, token, bulkLog);
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
 
         protected override void Append(LoggingEvent loggingEvent)
         {
---
+++ Err0Http.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+
+namespace err0.log4net
+{
+    public class Err0Http
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private static int inFlight = 0;
+        private static long errorUntil = 0;
+
+        public static bool canCall()
+        {
+            return inFlight < 4;
+        }
+
+        public static async void call(Uri uri, string token, JObject payload)
+        {
+            if (errorUntil != 0)
+            {
+                long now = DateTime.UtcNow.Ticks;
+                if (now < errorUntil)
+                {
+                    return; // silently drops logs, there is an error on http.
+                }
+            }
+
+            Interlocked.Increment(ref inFlight);
+
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                RequestUri = uri,
+                Method = HttpMethod.Post
+            };
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            request.Content = new StringContent(payload.ToString(), Encoding.UTF8, "application/json");
+            var result = await client.SendAsync(request);
+            Console.Out.WriteLine(result.StatusCode);
+            Interlocked.Decrement(ref inFlight);
+            if (result.IsSuccessStatusCode)
+            {
+                Interlocked.Exchange(ref errorUntil, 0);
+            }
+            else
+            {
+                Interlocked.Exchange(ref errorUntil, DateTime.UtcNow.Ticks + 30 * 60 * TimeSpan.TicksPerSecond);
+            }
+        }
+    }
+}
